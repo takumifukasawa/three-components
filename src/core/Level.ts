3b251f8ca698f773bb919ca41246858c23a5adc8
takumifukasawa <fukapon912@gmail.com>
2019-10-25 17:34:10 +0900

    add core modules

---
+++ src/core/Level.ts
@@ -0,0 +1,77 @@
+import * as THREE from "three";
+import Component from "../components/Component";
+import Tag from "./Tag";
+import * as _ from "lodash";
+import CameraComponent from "../components/CameraComponent";
+import { Actor } from "..";
+import { EventEmitter } from 'events';
+// import strEnum from "../utils/strEnum";
+
+class Level {
+  public scene: THREE.Scene;
+  public events: EventEmitter = new EventEmitter();
+
+  private actors: Actor[] = [];
+  private mainCamera: THREE.Camera | undefined;
+
+  constructor(scene: THREE.Scene) {
+    this.scene = scene;
+  }
+
+  getActorByTag(tag: Tag): Actor | undefined {
+    return _.find(this.actors, actor => actor.tag == tag);
+  }
+
+  // TODO: levelでやる必要ある？
+  setMainCamera(): void {
+    const actor = this.getActorByTag(Tag.MainCamera);
+    if(!actor) return;
+    const cameraComponent = actor.getComponent(CameraComponent);
+    if(!cameraComponent) return;
+    this.mainCamera = cameraComponent.camera;
+  }
+
+  getMainCamera(): THREE.Camera | undefined {
+    return this.mainCamera;
+  }
+
+  addActor(actor: Actor) {
+    this.actors.push(actor);
+    this.scene.add(actor.transform);
+  }
+
+  async load() {
+    return Promise.all(_.map(this.actors, async actor => {
+      const loader = async () => {
+        await actor.load();
+      };
+      await loader();
+    }));
+  }
+
+  awake() {
+    this.setMainCamera();
+    _.forEach(this.actors, actor => {
+      actor.awake();
+    });
+  }
+
+  start() {
+    _.forEach(this.actors, actor => actor.start());
+  }
+
+  setSize(width: number, height: number) {
+    _.forEach(this.actors, actor => actor.setSize(width, height));
+  }
+
+  // accumulation
+  fixedUpdate(time: number, deltaTime: number) {
+    _.forEach(this.actors, actor => actor.fixedUpdate(time, deltaTime));
+  }
+  // skipper update
+  update(time: number, deltaTime: number) {
+    _.forEach(this.actors, actor => actor.update(time, deltaTime));
+  }
+}
+
+export default Level;
---
+++ src/core/Tag.ts
@@ -0,0 +1,5 @@
+enum Tag {
+  None,
+  MainCamera,
+};
+export default Tag;
